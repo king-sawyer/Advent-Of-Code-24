4cc1c480588ff06497263a627b013c4ccc821e04
king-sawyer <kingsawyer555@gmail.com>
2025-01-17 07:13:44 -0800

    Added comments describing problem and solution

--- Puzzle1/part2.py
+++ Puzzle1/part2.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
+"""
+Puzzle Description: 
+Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+"""
+
 def puzzle1Part2():
 
     data = pd.read_csv('data.txt', sep="   ", engine='python', header=None)
@@ -14,8 +19,10 @@ def puzzle1Part2():
 
     similarity = 0
 
+    "To get the total number of occurances for each value in the second array I am utilizing a Counter which is a a dict subclass. It returns a Counter containing each value and the total number of occurances of the value in the array"
     occurrances = Counter(sortedList2)
 
+    "I iterate over each value in the first list then search the occurances Counter by using the first lists current value as the key to Counter to get the total occurances. Then I multiply that by the first lists current value"
     for i in range(len(sortedList1)):
         similarity += ((occurrances[sortedList1[i]]) * sortedList1[i] )
 
