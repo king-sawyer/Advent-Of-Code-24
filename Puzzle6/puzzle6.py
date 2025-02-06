0f28d3143b8da33d7146e9cf8902e308fa700dc0
king-sawyer <kingsawyer555@gmail.com>
2025-02-05 21:44:38 -0800

    Completed 6.1, needs to be refactored as I exceeded pythons recursion limit and set it manually

--- Puzzle6/puzzle6.py
+++ Puzzle6/puzzle6.py
@@ -1,37 +1,52 @@
 WIDTH = 0
 HEIGHT = 0
 
-def part_1(data, start_position, direction) -> bool:
+import sys
+sys.setrecursionlimit(50000)
 
-    if start_position[0] < WIDTH or start_position[1] < HEIGHT:
-        return True
+def part_1(data, start_position, direction, visited_locations) -> int:
 
-    next_x, next_y = 1
+    next_col = next_row = 1
     next_direction = ''
 
     match direction:
         case 'u':
-            next_x = 0
-            next_y = 1
+            next_col = 0
+            next_row = -1
             next_direction = 'r'
         case 'r': 
-            next_x = 1
-            next_y = 0
+            next_col = 1
+            next_row = 0
             next_direction = 'd'
         case 'd':
-            next_x = 0
-            next_y = -1
+            next_col = 0
+            next_row = 1
             next_direction = 'l'
         case 'l':
-            next_x = -1
-            next_y = 0
+            next_col = -1
+            next_row = 0
             next_direction = 'u'
 
-    # If next position is an x
-    # If next position is out of bounds
-    # otherwise move forward
+    new_start_position = [start_position[0] + next_row, start_position[1] + next_col]
 
-    return False
+    if (new_start_position[0]) >= HEIGHT or (new_start_position[0]) < 0 or (new_start_position[1]) >= WIDTH or (new_start_position[1]) < 0:
+        return 1
+    
+    visited_locations[start_position[0]][start_position[1]] = 'X'
+
+    # If next position is an #
+    if data[new_start_position[0]][new_start_position[1]] == '#':
+        
+        return part_1(data, start_position, next_direction, visited_locations)
+    # Move
+    else:
+        
+        if visited_locations[new_start_position[0]][new_start_position[1]] == 'X':
+            return part_1(data, new_start_position, direction, visited_locations) 
+        else:
+            return part_1(data, new_start_position, direction, visited_locations) + 1
+
+
 
 def part_2() -> bool:
     
@@ -39,7 +54,7 @@ def part_2() -> bool:
     return False
 
 def main() -> bool:
-    with open('ex.txt', 'r') as file:
+    with open('data.txt', 'r') as file:
             data = [line.strip() for line in file.readlines()]
 
     global WIDTH 
@@ -50,17 +65,19 @@ def main() -> bool:
 
     direction = 'u'
     starting_position = []
+    part_1_result = 0
+    visited_locations = [['.' for _ in range(WIDTH)] for _ in range(HEIGHT)]
 
     for i in range(len(data)):
         for j in range(len(data[i])):
             if data[i][j] == '^':
                  starting_position = [i,j]
 
-    part_1(data,starting_position,direction)
 
-    print(WIDTH)
-    print(HEIGHT)
-    print(starting_position)
+
+    part_1_result = part_1(data, starting_position, direction, visited_locations)
+
+    print(part_1_result)
 
     return False
 
